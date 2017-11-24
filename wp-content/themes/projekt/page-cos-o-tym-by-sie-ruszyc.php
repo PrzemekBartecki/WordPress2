c10c960503cf0286f404c33ddf689473ead85326
Przemek Bartecki <przemyslaw.bartecki@gmail.com>
2017-11-24 14:02:24 +0100

    wersja pokazowa

--- wp-content/themes/projekt/page-cos-o-tym-by-sie-ruszyc.php
+++ wp-content/themes/projekt/page-cos-o-tym-by-sie-ruszyc.php
@@ -4,6 +4,9 @@
 		<?php if ( have_posts() ) : while ( have_posts() ) :    the_post(); ?>
 		  <!-- post -->
 			<?php the_content();?>
+	<div class="videoWrapper">
+		<iframe width="640" height="480" src="https://www.youtube.com/embed/TA6ykF-WVc4?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
+	</div>
 		<?php endwhile; ?>
 		  <!-- post navigation -->
 		<?php else: ?>
