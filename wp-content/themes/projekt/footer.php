75907ade567db77963b3ec145229b8a81314022b
Przemek Bartecki <przemyslaw.bartecki@gmail.com>
2017-11-29 16:18:05 +0100

    footer na dole

--- wp-content/themes/projekt/footer.php
+++ wp-content/themes/projekt/footer.php
@@ -1,18 +1,10 @@
 
 <footer >
-
-		<section class="container ">
-
-			<p>Copyright © 2017</p>
-			<p>Projekt i wykonanie: <a href="https://github.com/PrzemekBartecki" title="profil GitHub"> Przemysław Bartecki.</a></p>
-			<p>Wszystkie prawa zastrzeżone.</p>
-
-		</section>
-
+	<p>Copyright © 2017</p>
+	<p>Projekt i wykonanie: <a href="https://github.com/PrzemekBartecki" title="profil GitHub"> Przemysław Bartecki.</a></p>
+	<p>Wszystkie prawa zastrzeżone.</p>
 </footer>
-
-
 </body>
-
 <?php	wp_footer(); ?>
+
 </html>
