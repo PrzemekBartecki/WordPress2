a1e20d092c5c3b5bb92e02abee03122e5f68001c
Przemek Bartecki <przemyslaw.bartecki@gmail.com>
2017-12-01 21:01:58 +0100

    poprawa padiiggów i footera i hedera dla mobie

--- wp-content/themes/projekt/page-kreatywnosc.php
+++ wp-content/themes/projekt/page-kreatywnosc.php
@@ -6,7 +6,7 @@
 	<a href="#zasady"> Q kreatywnosci </a>
 	<a href="#etapy"> Etapy procesu twórczego </a>
 	<br>
-	abababababababahahshrjfhhghghghhabababababababahahshrjfhhghghghhabababababababahahshrjfhhghghghhababababababa
+	<p>abababababababahahshrjfhhghghghhabababababababahahshrjfhhghghghhabababababababahahshrjfhhghghghhababababababa
 	bahahshrjfhhghghghhabababababababahahshrjfhhghghghhabababababababahahshrjfhhghghghhabababababababahahshrjfhhg
 	hghghhabababababababahahshrjfhhghghghhabababababababahahshrjfhhghghghhabababababababahahshrjfhhghghghhabababa
 	babababahahshrjfhhghghghhabababababababahahshrjfhhghghghhabababababababahahshrjfhhghghghhabababababababahahsh
@@ -174,11 +174,11 @@
 	rjfhhghghghhabababababababahahshrjfhhghghghhabababababababahahshrjfhhghghghhabababababababahahshrjfhhghghghha
 	bababababababahahshrjfhhghghghhabababababababahahshrjfhhghghghhabababababababahahshrjfhhghghghhabababababababa
 	hahshrjfhhghghghhabababababababahahshrjfhhghghghhabababababababahahshrjfhhghghghhabababababababahahshrjfhhghghg
-	hh
+		hh</p>
 	<section style="color:red; min-height:100vh" id="wstep"> >
 
 
-		Program Destination Imagination tworzą zespoły dzieci lub młodzieży, liczące od 4 do 7 osób, wraz z Trenerami. Drużyny powstają w przedszkolach, szkołach, ośrodkach edukacji, domach kultury, bibliotekach i wielu innych miejscach, gdzie odbywają się różnego rodzaju zajęcia pozalekcyjne.
+		<p>Program Destination Imagination tworzą zespoły dzieci lub młodzieży, liczące od 4 do 7 osób, wraz z Trenerami. Drużyny powstają w przedszkolach, szkołach, ośrodkach edukacji, domach kultury, bibliotekach i wielu innych miejscach, gdzie odbywają się różnego rodzaju zajęcia pozalekcyjne.
 
 
 		Drużyny w ciągu roku szkolnego rozwijają swoją kreatywność, ćwiczą umiejętność pracy zespołowej oraz twórczego rozwiązywania problemów w praktyce wykorzystując nowatorskie metody i techniki myślenia twórczego. Zdobytą wiedzę i umiejętności sprawdzają w nauce szkolnej, a stworzone przez siebie rozwiązania prezentują podczas Ogólnopolskiej Olimpiady Kreatywności. Zwycięskie drużyny reprezentują Polskę podczas Światowej Olimpiady Kreatywności w Stanach Zjednoczonych.
@@ -674,9 +674,9 @@
 		(D.R. WIDDER, Uniwersytet Filadelfia
 		<br>
 
-
+		</p>>
 	</section>
-	abababababababahahshrjfhhghghghhabababababababahahshrjfhhghghghhabababababababahahshrjfhhghghghhababababababa
+	<p>abababababababahahshrjfhhghghghhabababababababahahshrjfhhghghghhabababababababahahshrjfhhghghghhababababababa
 	bahahshrjfhhghghghhabababababababahahshrjfhhghghghhabababababababahahshrjfhhghghghhabababababababahahshrjfhhg
 	hghghhabababababababahahshrjfhhghghghhabababababababahahshrjfhhghghghhabababababababahahshrjfhhghghghhabababa
 	babababahahshrjfhhghghghhabababababababahahshrjfhhghghghhabababababababahahshrjfhhghghghhabababababababahahsh
@@ -844,7 +844,7 @@
 	rjfhhghghghhabababababababahahshrjfhhghghghhabababababababahahshrjfhhghghghhabababababababahahshrjfhhghghghha
 	bababababababahahshrjfhhghghghhabababababababahahshrjfhhghghghhabababababababahahshrjfhhghghghhabababababababa
 	hahshrjfhhghghghhabababababababahahshrjfhhghghghhabababababababahahshrjfhhghghghhabababababababahahshrjfhhghghg
-	hh
+		hh</p>
 	<?php if ( have_posts() ) : while ( have_posts() ) :    the_post(); ?>
 		<!-- post -->
 		<?php the_post_thumbnail();?>
