9a64172880fdf9575f1ee4c2bfa15d300d07e02d
Przemek Bartecki <przemyslaw.bartecki@gmail.com>
2017-12-04 20:49:19 +0100

    potwierdzenie formularz2a

--- wp-content/themes/projekt/page-kontakt.php
+++ wp-content/themes/projekt/page-kontakt.php
@@ -6,7 +6,7 @@
 <?php if ( have_posts() ) : while ( have_posts() ) :    the_post(); ?>
 	<!-- post -->
 	<?php the_content(); ?>
-	<div class="g-recaptcha" data-sitekey="6LcFoDsUAAAAALB_OJtH0m336t_-Az6YH_WlLIau"></div>
+
 <?php endwhile; ?>
 	<!-- post navigation -->
 <?php else: ?>
