df38a7e4fc93e831506138840e918960bc1fbea3
Przemek Bartecki <przemyslaw.bartecki@gmail.com>
2017-12-04 13:59:07 +0100

    test 22

--- wp-content/themes/projekt/header.php
+++ wp-content/themes/projekt/header.php
@@ -5,7 +5,7 @@
 	<meta name="description" content="Chcesz wiedzieć co zrobić by ruszyć z miejsca, potrzebujesz informacji na temat jakiś tam">
 	<meta name="keywords" content="rozwój, edukacja, zdrowie, finnase">
 	<meta name="viewport"
-		  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+		  content="width=device-width, user-scalable=no, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>
 		<?php	echo	get_bloginfo('name');	?>
