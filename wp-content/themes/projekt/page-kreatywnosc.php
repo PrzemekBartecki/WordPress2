9cd30a5012e315c609b13ee89bdcb72529a75e7f
Przemek Bartecki <przemyslaw.bartecki@gmail.com>
2017-12-05 18:06:25 +0100

    strona kreatywnospoprawki

--- wp-content/themes/projekt/page-kreatywnosc.php
+++ wp-content/themes/projekt/page-kreatywnosc.php
@@ -2,7 +2,7 @@
 
 <section class="container">
 
-	<section id="kreoMenu" class="creative">
+	<section id="kreoMenu" >
 		<div class="menuKreatywnosc">
 			<a href="#wstep"> Wstęp </a>
 			<a href="#zasady"> Q kreatywnosci </a>
@@ -10,136 +10,11 @@
 		</div>
 	</section>
 	<div class="space"></div>
-	<section class="sizeContent">
-				<p>Witam</p>
-	</section>
 
-	<section id="wstep">
-
-
-			<p>Program Destination Imagination tworzą zespoły dzieci lub młodzieży, liczące od 4 do 7 osób, wraz z Trenerami. Drużyny powstają w przedszkolach, szkołach, ośrodkach edukacji, domach kultury, bibliotekach i wielu innych miejscach, gdzie odbywają się różnego rodzaju zajęcia pozalekcyjne.
-
-
-			Drużyny w ciągu roku szkolnego rozwijają swoją kreatywność, ćwiczą umiejętność pracy zespołowej oraz twórczego rozwiązywania problemów w praktyce wykorzystując nowatorskie metody i techniki myślenia twórczego. Zdobytą wiedzę i umiejętności sprawdzają w nauce szkolnej, a stworzone przez siebie rozwiązania prezentują podczas Ogólnopolskiej Olimpiady Kreatywności. Zwycięskie drużyny reprezentują Polskę podczas Światowej Olimpiady Kreatywności w Stanach Zjednoczonych.
-
-
-			Drużyny podzielone są na poziomy wiekowe:
-			Wschodzące Gwiazdy - przedszkole
-
-			I – od 1 do 5 klasy szkoły podstawowej
-			II - 6 klasa szkoły podstawowej oraz I, II klasa gimnazjum
-			III - III Drużyny w ciągu roku szkolnego przechodzą przez proces twórczy „From imagination to innovation – od wyobraźni do innowacji”. Uczą się zarządzania projektami i efektywnej pracy zespołowej.
-
-
-
-			„Dzisiejsi uczniowie kilka razy w swoim życiu zmienią zawód, dlatego będą potrzebowali różnorodnych umiejętności. Celem kształcenia nie jest jedynie zapamiętywanie informacji i zdobywanie wiedzy. Jest nim nauka pracy w zespole, nauka „uczenia się” i zasady DI: współpraca, myślenie krytyczne i twórcze rozwiązywanie problemów”.
-
-			(D.R. WIDDER, Uniwersytet Filadelfia
-			<br>Program Destination Imagination tworzą zespoły dzieci lub młodzieży, liczące od 4 do 7 osób, wraz z Trenerami. Drużyny powstają w przedszkolach, szkołach, ośrodkach edukacji, domach kultury, bibliotekach i wielu innych miejscach, gdzie odbywają się różnego rodzaju zajęcia pozalekcyjne.
-
-
-			Drużyny w ciągu roku szkolnego rozwijają swoją kreatywność, ćwiczą umiejętność pracy zespołowej oraz twórczego rozwiązywania problemów w praktyce wykorzystując nowatorskie metody i techniki myślenia twórczego. Zdobytą wiedzę i umiejętności sprawdzają w nauce szkolnej, a stworzone przez siebie rozwiązania prezentują podczas Ogólnopolskiej Olimpiady Kreatywności. Zwycięskie drużyny reprezentują Polskę podczas Światowej Olimpiady Kreatywności w Stanach Zjednoczonych.
-
-
-			Drużyny podzielone są na poziomy wiekowe:
-			Wschodzące Gwiazdy - przedszkole
-
-			I – od 1 do 5 klasy szkoły podstawowej
-			II - 6 klasa szkoły podstawowej oraz I, II klasa gimnazjum
-			III - III klasa gimnazjum, klasy ponadgimnazjalne
-			Poziom Uniwersytecki - studenci
-
-			​
-
-			Każda drużyna jest prowadzona przez Trenera-nauczyciela, który odbył szkolenie.
-
-			Trenerzy motywują i inspirują drużynę, ale nigdy nie dają gotowych rozwiązań, nie są częścią procesu twórczego, podczas którego tworzone są pomysły i wdrażane rozwiązania.
-
-			​
-
-			Drużyny w ciągu roku szkolnego przechodzą przez proces twórczy „From imagination to innovation – od wyobraźni do innowacji”. Uczą się zarządzania projektami i efektywnej pracy zespołowej.
-
-
-
-			„Dzisiejsi uczniowie kilka razy w swoim życiu zmienią zawód, dlatego będą potrzebowali różnorodnych umiejętności. Celem kształcenia nie jest jedynie zapamiętywanie informacji i zdobywanie wiedzy. Jest nim nauka pracy w zespole, nauka „uczenia się” i zasady DI: współpraca, myślenie krytyczne i twórcze rozwiązywanie problemów”.
-
-			(D.R. WIDDER, Uniwersytet Filadelfia
-			<br>Program Destination Imagination tworzą zespoły dzieci lub młodzieży, liczące od 4 do 7 osób, wraz z Trenerami. Drużyny powstają w przedszkolach, szkołach, ośrodkach edukacji, domach kultury, bibliotekach i wielu innych miejscach, gdzie odbywają się różnego rodzaju zajęcia pozalekcyjne.
-
-
-			Drużyny w ciągu roku szkolnego rozwijają swoją kreatywność, ćwiczą umiejętność pracy zespołowej oraz twórczego rozwiązywania problemów w praktyce wykorzystując nowatorskie metody i techniki myślenia twórczego. Zdobytą wiedzę i umiejętności sprawdzają w nauce szkolnej, a stworzone przez siebie rozwiązania prezentują podczas Ogólnopolskiej Olimpiady Kreatywności. Zwycięskie drużyny reprezentują Polskę podczas Światowej Olimpiady Kreatywności w Stanach Zjednoczonych.
-
-
-			Drużyny podzielone są na poziomy wiekowe:
-			Wschodzące Gwiazdy - przedszkole
-
-			„Dzisiejsi uczniowie kilka razy w swoim życiu zmienią zawód, dlatego będą potrzebowali różnorodnych umiejętności. Celem kształcenia nie jest jedynie zapamiętywanie informacji i zdobywanie wiedzy. Jest nim nauka pracy w zespole, nauka „uczenia się” i zasady DI: współpraca, myślenie krytyczne i twórcze rozwiązywanie problemów”.
-
-			(D.R. WIDDER, Uniwersytet Filadelfia
-			<br>Program Destination Imagination tworzą zespoły dzieci lub młodzieży, liczące od 4 do 7 osób, wraz z Trenerami. Drużyny powstają w przedszkolach, szkołach, ośrodkach edukacji, domach kultury, bibliotekach i wielu innych miejscach, gdzie odbywają się różnego rodzaju zajęcia pozalekcyjne.
-
-
-			Drużyny w ciągu roku szkolnego rozwijają swoją kreatywność, ćwiczą umiejętność pracy zespołowej oraz twórczego rozwiązywania problemów w praktyce wykorzystując nowatorskie metody i techniki myślenia twórczego. Zdobytą wiedzę i umiejętności sprawdzają w nauce szkolnej, a stworzone przez siebie rozwiązania prezentują podczas Ogólnopolskiej Olimpiady Kreatywności. Zwycięskie drużyny reprezentują Polskę podczas Światowej Olimpiady Kreatywności w Stanach Zjednoczonych.
-
-
-			Drużyny podzielone są na poziomy wiekowe:
-			Wschodzące Gwiazdy - przedszkole
-
-			I – od 1 do 5 klasy szkoły podstawowej
-			II - 6 klasa szkoły podstawowej oraz I, II klasa gimnazjum
-			III - III klasa gimnazjum, klasy ponadgimnazjalne
-			Poziom Uniwersytecki - studenci
-
-			​
-
-			Każda drużyna jest prowadzona przez Trenera-nauczyciela, który odbył szkolenie.
-
-			Trenerzy motywują i inspirują drużynę, ale nigdy nie dają gotowych rozwiązań, nie są częścią procesu twórczego, podczas którego tworzone są pomysły i wdrażane rozwiązania.
-
-			​
-
-			Drużyny w ciągu roku szkolnego przechodzą przez proces twórczy „From imagination to innovation – od wyobraźni do innowacji”. Uczą się zarządzania projektami i efektywnej pracy zespołowej.
-
-
-
-			„Dzisiejsi uczniowie kilka razy w swoim życiu zmienią zawód, dlatego będą potrzebowali różnorodnych umiejętności. Celem kształcenia nie jest jedynie zapamiętywanie informacji i zdobywanie wiedzy. Jest nim nauka pracy w zespole, nauka „uczenia się” i zasady DI: współpraca, myślenie krytyczne i twórcze rozwiązywanie problemów”.
-
-			(D.R. WIDDER, Uniwersytet Filadelfia
-			<br>Program Destination Imagination tworzą zespoły dzieci lub młodzieży, liczące od 4 do 7 osób, wraz z Trenerami. Drużyny powstają w przedszkolach, szkołach, ośrodkach edukacji, domach kultury, bibliotekach i wielu innych miejscach, gdzie odbywają się różnego rodzaju zajęcia pozalekcyjne.
-
-
-			Drużyny w ciągu roku szkolnego rozwijają swoją kreatywność, ćwiczą umiejętność pracy zespołowej oraz twórczego rozwiązywania problemów w praktyce wykorzystując nowatorskie metody i techniki myślenia twórczego. Zdobytą wiedzę i umiejętności sprawdzają w nauce szkolnej, a stworzone przez siebie rozwiązania prezentują podczas Ogólnopolskiej Olimpiady Kreatywności. Zwycięskie drużyny reprezentują Polskę podczas Światowej Olimpiady Kreatywności w Stanach Zjednoczonych.
-
-
-			Drużyny podzielone są na poziomy wiekowe:
-			Wschodzące Gwiazdy - przedszkole
-
-			I – od 1 do 5 klasy szkoły podstawowej
-			II - 6 klasa szkoły podstawowej oraz I, II klasa gimnazjum
-			III - III klasa gimnazjum, klasy ponadgimnazjalne
-			Poziom Uniwersytecki - studenci
-
-			​
-
-			Każda drużyna jest prowadzona przez Trenera-nauczyciela, który odbył szkolenie.
-
-			Trenerzy motywują i inspirują drużynę, ale nigdy nie dają gotowych rozwiązań, nie są częścią procesu twórczego, podczas którego tworzone są pomysły i wdrażane rozwiązania.
-
-			​
-
-			Drużyny w ciągu roku szkolnego przechodzą przez proces twórczy „From imagination to innovation – od wyobraźni do innowacji”. Uczą się zarządzania projektami i efektywnej pracy zespołowej.
-
-
-
-			„Dzisiejsi uczniowie kilka razy w swoim życiu zmienią zawód, dlatego będą potrzebowali różnorodnych umiejętności. Celem kształcenia nie jest jedynie zapamiętywanie informacji i zdobywanie wiedzy. Jest nim nauka pracy w zespole, nauka „uczenia się” i zasady DI: współpraca, myślenie krytyczne i twórcze rozwiązywanie problemów”.
-
-			(D.R. WIDDER, Uniwersytet Filadelfia
-			<br>
-
-			</p>>
-		</section>
-
-		<?php if ( have_posts() ) : while ( have_posts() ) :    the_post(); ?>
+<!--	***************************************************-->
+<!--	================ pętla WP =========================-->
+<!--	***************************************************-->
+	<?php if ( have_posts() ) : while ( have_posts() ) :    the_post(); ?>
 		<!-- post -->
 		<?php the_post_thumbnail();?>
 		<p><?php the_content();?></p>
@@ -152,7 +27,19 @@
 	<?php endif; ?>
 
 
+	<section class="sizeContent" id="wstep">
+		<p>Tekst wprowadzajcy do rkeatynwsoci</p>
 
+
+	</section>
+	<section class="sizeContent" id="zasady">
+
+			<h2>A tu bedzie o zasadach</h2>
+	</section>
+
+	<section class="sizeContent" id="etapy">
+		<h2>Etapy procesu twórczego</h2>
+	</section>
 
 </section>
 <?php get_footer(); ?>
