272f8d00b18bb9e5f3fc01a7f26c1738f6c6d268
Przemek Bartecki <przemyslaw.bartecki@gmail.com>
2017-12-05 10:02:45 +0100

    weryfikacja

--- wp-content/themes/projekt/header.php
+++ wp-content/themes/projekt/header.php
@@ -7,6 +7,7 @@
 	<meta name="viewport"
 		  content="width=device-width, user-scalable=no, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<meta name="google-site-verification" content="S827nZBxLJkwFknYwyFsLf2lIWt7QUKD5xUkv05RAkg" />
 	<title>
 		<?php	echo	get_bloginfo('name');	?>
 	</title>
