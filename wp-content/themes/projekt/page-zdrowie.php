d9113027626eb4a3863fe2883fce67482a900fac
Przemek Bartecki <przemyslaw.bartecki@gmail.com>
2017-12-12 16:02:38 +0100

    zdrowie zakonczenie

--- wp-content/themes/projekt/page-zdrowie.php
+++ wp-content/themes/projekt/page-zdrowie.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 <section class="container ">
-	<article class="container  healty">
+	<article class=" healty ">
 		<div class="readTxt">
 			<?php if ( have_posts() ) : while ( have_posts() ) :    the_post(); ?>
 				<!-- post -->
@@ -15,9 +15,111 @@
 			<?php endif; ?>
 		</div>
 		<a style="background-color:black; float:right;color:white;text-decoration:none;padding:4px 6px;font-family:-apple-system, BlinkMacSystemFont, &quot;San Francisco&quot;, &quot;Helvetica Neue&quot;, Helvetica, Ubuntu, Roboto, Noto, &quot;Segoe UI&quot;, Arial, sans-serif;font-size:12px;font-weight:bold;line-height:1.2;display:inline-block;border-radius:3px;" href="https://unsplash.com/@jamie452?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge" target="_blank" rel="noopener noreferrer" title="Download free do whatever you want high-resolution photos from Jamie Street"><span style="display:inline-block;padding:2px 3px;"><svg xmlns="http://www.w3.org/2000/svg" style="height:12px;width:auto;position:relative;vertical-align:middle;top:-1px;fill:white;" viewBox="0 0 32 32"><title>unsplash-logo</title><path d="M20.8 18.1c0 2.7-2.2 4.8-4.8 4.8s-4.8-2.1-4.8-4.8c0-2.7 2.2-4.8 4.8-4.8 2.7.1 4.8 2.2 4.8 4.8zm11.2-7.4v14.9c0 2.3-1.9 4.3-4.3 4.3h-23.4c-2.4 0-4.3-1.9-4.3-4.3v-15c0-2.3 1.9-4.3 4.3-4.3h3.7l.8-2.3c.4-1.1 1.7-2 2.9-2h8.6c1.2 0 2.5.9 2.9 2l.8 2.4h3.7c2.4 0 4.3 1.9 4.3 4.3zm-8.6 7.5c0-4.1-3.3-7.5-7.5-7.5-4.1 0-7.5 3.4-7.5 7.5s3.3 7.5 7.5 7.5c4.2-.1 7.5-3.4 7.5-7.5z"></path></svg></span><span style="display:inline-block;padding:2px 3px;">Jamie Street</span></a>
-
 	</article>
+
+	<article class="shortInfo">
+		<h3>KWASY OMEGA</h3>
+		<p>
+			Inspiracją jest naturą. Juice Plus+ Omega Blend to kombinacja kwasów tłuszczowych omega 3, 5, 6, 7 i 9
+			pochodzenia roślinnego, pozyskanych z nasion, owoców i alg. Aby utrzymać wysoką jakość naturalnych
+			składników oleje są jedynie minimalnie przetwarzane.
+		</p>
+		<p>
+			Źródłem pochodzenia kwasów tłuszczowych natura. W porównaniu do innych suplementów dostępnych na rynku,
+			kwasy tłuszczowe Omega 3 w Juice Plus+ Omega nie pochodzą z ryb. Czyste zrównoważone, wegańskie:
+			pomijamy ryby i przechodzimy od razu do źródła pochodzenia.
+		</p>
+		<p>
+			Idealne połączenie natury i innowacji. Wysoce nowoczesny proces i wartościowe oleje zawarte w
+			wegańskiej kapsułce stworzonej z skrobii tapioki. Wysokie temperatury nie są konieczne do połączenia
+			dwóch połówek, zapewniając jednocześnie, że substancje odżywcze zachowują swoją stabilność.
+		</p>
+		<p>
+			Juice Plus+ Omega Blend: składniki w 100% wolne od GMO, produkt bezglutenowy, nie zawiera sztucznych
+			barwników, smaków i konserwantów, certyfikowany produkt koszerny.
+		</p>
+	</article>
+
+	<div class="omegaBg"></div>
+	<article class="shortInfo">
+		<h3>NAUKOWA i BADANIA</h3>
+		<p>
+			Od 20 lat produkty Juice PLUS + są poddawane badaniom na renomowanych uniwersytetach i w instytucjach
+			badawczych na całym świecie. Wyniki tych badań są regularnie publikowane w czasopismach naukowych dotyczących
+			żywienia. Juice PLUS+ zawiera składniki odżywcze, które potwierdzają  prozdrowotne efekty ich działania.
+		</p>
+	</article>
+	<div class="studyBg"></div>
+	<article class="shortInfo">
+		<h3>Children's Health Study</h3>
+		<h4>BADANIA NAD ZDROWIEM DZIECI</h4>
+		<p>
+			Dieta bogata w błonnik, złożona ze świeżych owoców i warzyw, jest zdrowsza niż jedzenie typu fast food,
+			ale co zrobić, gdy Twoje dzieci męczą Cię, abyś kupił im pizzę lub hamburgera? Bądź gdy nie możesz poradzić
+			sobie z kolejnym napadem złości, gdy maluch chce słodycz zamiast  pełnoziarnistego herbatnika.
+		</p>
+		<p>
+			Otyłość stopniowo staje się poważnym problemem wynikającym z poniższych tendencji; dotyczy to nie tylko
+			dzieci, lecz również dorosłych:</p>
+		<ol>
+			<li>Tendencja do zmniejszonej aktywności fizycznej</li>
+			<li>Zwiększone spożycie wysokoenergetycznych produktów spożywczych, które obfitują w tłuszcz i cukry</li>
+			<li>96% dzieci w wieku 6 do 12 lat je zbyt mało warzyw a  81% zbyt mało owoców. Ponad 70% młodzieży je zbyt mało owoców i warzyw.</li>
+			<li>Aby rozwijać zdrowe nawyki, większość dzieci potrzebuje wsparcia</li>
+		</ol>
+		<p>
+			To dlatego Juice PLUS+ wprowadziło badanie nad zdrowiem dzieci, aby wspierać rodziny w przyjmowaniu zdrowszej diety i trybu życia.
+
+			Badanie nad zdrowiem dzieci Juice PLUS+ to przeprowadzone na całym świecie w ciągu kilku lat badanie obserwacyjne, w którym uczestniczy milion uczestników. Poświęcone jest ono zdrowemu odżywianiu dzieci
+		</p>
+		<div class="chsBg"></div>
+		<h4>Jaki cel ma badanie nad zdrowiem dzieci Juice PLUS+?</h4>
+		<p>
+			Celem naszego badania jest poprawa ogólnego sposobu odżywiania i nawyków żywieniowych wśród dzieci oraz
+			całych rodzin. Dlatego zadaliśmy sobie następujące pytania:
+		</p>
+		<ol>
+			<li>Czy dobre odżywianie zachęca do innych wyborów w kwestii zdrowego trybu życia?</li>
+			<li>Jaki jest najlepszy okres dzieciństwa na wprowadzenie dobrych nawyków zdrowotnych i żywieniowych?</li>
+			<li>Czy zaangażowanie rodziców jest jedynym kluczem do przyjęcia przez dzieci zdrowych nawyków?</li>
+		</ol>
+		<p>
+			To, co do tej pory udało nam się już odkryć wraz z rodzinami, które rozpoczęły badanie nad zdrowiem dzieci, to fakt, że zaczęły one przyglądać się również innym obszarom swego trybu życia. Zaczęły pić więcej wody, spożywać mniej słodyczy i jedzenia typu fast food.
+		</p>
+	</article>
+	<div class="videoWrapper ">
+		<iframe width="640" height="480" src="https://www.youtube.com/embed/wmbqiDEudXM" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+	</div>
+	<article class="shortInfo">
+		<h3>Moje - 90 - dni</h3>
+		<p>
+			Moje 90 dni jest częscią One Simple Change, idei mówiącej, o tym, że jedna mała zmiana może przynieść
+			niespodziewane efekty. Każdy z nas chciałby prowadzić zdrowy trybu życia. Znalezienie odpowiedniego momentu
+			by zacząć, często okazuje się wyzwaniem. Właściwe odżywanie, regularne ćwiczenia i  czas dla rodziny,
+			przyjaciół i dla siebie mogą okazać się nieosiągalnym celem. Zamiast skupiać się na poprawie wszystkich
+			aspektów swojego zdrowia naraz, zacznij od wyznaczenia sobie realnych celów. Wprowadź jedną mała zmianę
+			jeszcze dziś.
+		</p>
+		<p>
+			Chętnie Ci w tym pomogę, nie zależnie czy chcesz zrzucić 5 - 10 -15 kg, czy też uzupłnić woją diętę o
+			brakujące składniki. Mam program, który został przygotowany przez specjalistów i daje realne efekty.
+			Chcesz poznać doświadczenia innych osób, które spróbowały, daj znać.
+		</p>
+	</article>
+	<section class="healty healty2">
+		<p>
+			Chcesz wiedzieć więcej<br>
+			<span><a href="http://bartecki.juiceplus.com" target="_blank">bartecki.juiceplus.com</a></span>
+		</p>
+		<p>
+			masz pytania napisz:<br>
+			<span><a href="mailto:przymslaw.bartecki@gmail.com">przymslaw.bartecki@gmail.com</a></span>
+		</p>
+
+	</section>
+
 </section>
+
 
 <?php get_footer(); ?>
 
