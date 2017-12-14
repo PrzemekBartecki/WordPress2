029a1f163ba26c1e1ba05095a16731e07ac6e043
Przemek Bartecki <przemyslaw.bartecki@gmail.com>
2017-12-15 00:16:42 +0100

    menu mobil dszis2

--- wp-content/themes/projekt/single.php
+++ wp-content/themes/projekt/single.php
@@ -1,11 +1,12 @@
 <?php get_header(); ?>
 <section class="container ">
+	<article class="readTxt">
 		<?php if ( have_posts() ) : while ( have_posts() ) :    the_post(); ?>
 			<!-- post -->
 			<h2><?php the_title(); ?></h2>
 			<?php the_content(); ?>
 			<div class="fb-like" data-href="<?php the_permalink() ?>" data-layout="standard" data-action="like" data-size="large" data-show-faces="true" data-share="true"></div>
-
+	</article>
 			<section class="navPost">
 				<div>
 					<strong><?php previous_post_link( '<strong>%link</strong>');?></strong>
